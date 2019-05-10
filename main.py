9e0c00ba39342be7184ab4e3cee4399287000f3e
Frank Li <zhao.lang.li2@gmail.com>
2019-05-10 01:50:27 -0500

    Update main.py
    Switched to Bezier curves

--- main.py
+++ main.py
@@ -1,120 +1,129 @@
-import numpy as np
-import tkinter
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import csv
-import random
-from PIL import Image
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import math
+import numpy as np
 
-if __name__=="__main__":
-    #GUI
-    #window=tkinter.Tk(className="2D Cubic Spline")
-    #window.mainloop()
+#Bezier Curves
+def linearBezier(p0, p1, t):
+    bez_val=((1-t)*p0)+(t*p1)
+    return bez_val
 
-    #size=15
-    #matrix=np.zeros((size, size), dtype=float)
+def quadraticBezier(p0, p1, p2, t):
+    bez_val=(math.pow((1-t), 2)*p0)+(2*(1-t)*t*p1)+(math.pow(t, 2)*p2)
+    return bez_val
 
-    input_file=open("input.txt")
-    x=[]
-    y=[]
-    for line in input_file:
-        point=line.split(",")
-        x.append(int(point[0]))
-        y.append(int(point[1]))
-        #matrix[size-x][size-y]=1
+def cubicBezier(p0, p1, p2, p3, t):
+    bez_val=(math.pow((1-t), 3)*p0)+(3*math.pow(1-t, 2)*t*p1)+(3*(1-t)*math.pow(t, 2)*p2)+(math.pow(t, 3)*p3)
+    return bez_val
 
-    new_y=y[0]+(8-x[0])/(x[1]-x[0])*(y[1]-y[0])
-    # print(new_y)
-    x.append(8)
-    y.append(new_y)
+list_x = []
+list_y = []
+list_z = []
 
-    #plt.scatter(x, y)
-    #plt.show()
-    input_file.close()
+new_x=[]
+new_y=[]
+new_z=[]
 
-    list_x=[]
-    list_y=[]
-    list_z=[]
-    size=[]
+size=[]
 
-    with open("smooth_path.csv", newline="") as csv_file:
-        path=csv.reader(csv_file, delimiter=" ")
+#append x y z coordinates to respective lists
+with open("smooth_path.csv", newline="") as csv_file:
+    path = csv.reader(csv_file, delimiter=" ")
 
-        for row in path:
-            row=row[0].split(",")
-            x=float(row[0])
-            y=float(row[1])
-            z=float(row[2])
-            list_x.append(x)
-            list_y.append(y)
-            list_z.append(z)
+    for row in path:
+        row = row[0].split(",")
+        x = float(row[0])
+        y = float(row[1])
+        z = float(row[2])
+        list_x.append(x)
+        list_y.append(y)
+        list_z.append(z)
 
-    with open("Gcorridor.csv", newline="") as csv_file:
-        corridor=csv.reader(csv_file, delimiter=" ")
+#append size to list
+with open("Gcorridor.csv", newline="") as csv_file:
+    corridor=csv.reader(csv_file, delimiter=" ")
 
-        for row in corridor:
-            row=row[0].split(",")
-            num=int(row[0])
-            s=float(row[1])
-            size.append(s)
+    for row in corridor:
+        row=row[0].split(",")
+        num=int(row[0])
+        s=float(row[1])
+        size.append(s*10)
 
-    fig=plt.figure()
-    ax=Axes3D(fig)
+# list_x=list_x[0:20]
+# list_y=list_y[0:20]
+# list_z=list_z[0:20]
 
-    list_x=list(list_x)
-    list_y=list(list_y)
-    list_z=list(list_z)
+for i in range(0, len(list_x)-2):
 
-    ax.scatter(list_x, list_y, list_z, s=size, marker="o")
-    plt.show()
+    # xval = linearBezier(list_x[i], list_x[i + 1], .5)
+    # yval = linearBezier(list_y[i], list_y[i + 1], .5)
+    # zval = linearBezier(list_z[i], list_z[i + 1], .5)
 
-    i=0
-    arr=np.zeros((256, 256), dtype=np.uint8)
+    # xval = quadraticBezier(list_x[i], list_x[i + 1], list_x[i+2], .5)
+    # yval = quadraticBezier(list_y[i], list_y[i + 1], list_y[i+2], .5)
+    # zval = quadraticBezier(list_z[i], list_z[i + 1], list_z[i+2], .5)
 
-    with open("MRcolorData1.txt", newline="") as csv_file:
-        colordata=csv.reader(csv_file, delimiter=" ")
+    # xval = cubicBezier(list_x[i], list_x[i + 1], list_x[i+2], list_x[i+3], .5)
+    # yval = cubicBezier(list_y[i], list_y[i + 1], list_y[i+2], list_y[i+3], .5)
+    # zval = cubicBezier(list_z[i], list_z[i + 1], list_z[i+2], list_z[i+3], .5)
 
-        for row in colordata:
+    x1=list_x[i]
+    x2=list_x[i+1]
+    x3=list_x[i+2]
 
-            row=row[0].split(",")
-            num=int(row[0])
-            x=int(row[1])
-            y=int(row[2])
-            intensity=int(row[3])
+    y1 = list_y[i]
+    y2 = list_y[i + 1]
+    y3 = list_y[i + 2]
 
-            arr[x][y]=intensity
+    z1 = list_z[i]
+    z2 = list_z[i + 1]
+    z3 = list_z[i + 2]
 
-            i+=1
+    for u in range(0, 10, 5):
+        j=u/10
 
-            if num==50:
-               break
+        xval = quadraticBezier(x1, x2, x3, j)
+        yval = quadraticBezier(y1, y2, y3, j)
+        zval = quadraticBezier(z1, z2, z3, j)
+        new_x.append(xval)
+        new_y.append(yval)
+        new_z.append(zval)
 
-        # im=Image.new("L", (144, 144))
-        # pix=im.load()
-        # for x in range(144):
-        #     for y in range(144):
-        #         pix[x,y]=0
+# list_x=[x+5 for x in list_x]
+# list_y=[x+5 for x in list_y]
+# list_z=[x-22 for x in list_z]
 
-        # im.save("test.png", "PNG")
-        print(arr)
-        img=Image.fromarray(arr, "L")
-        img.show()
-        img.save("test.png")
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(list_x, list_y, list_z, s=size, marker="o")
+ax.scatter(new_x, new_y, new_z, s=size, marker="o")
+plt.show()
 
-# import numpy as np
-# import matplotlib.pyplot as plt
+# x=[1, 3, 1]
+# y=[1, 3, 5]
 #
-# # generate some data
-# ax = np.arange(-9, 10)
-# X, Y = np.meshgrid(ax, ax)
-# print(X)
-# Z = X ** 2 + Y ** 2
+# for i in range(0, 10):
+#     x1=1
+#     y1=1
+#     x2=3
+#     y2=3
+#     x3=1
+#     y3=5
 #
-# # normalize the data and convert to uint8 (grayscale conventions)
-# zNorm = (Z - Z.min()) / (Z.max() - Z.min()) * 255
-# zNormUint8 = zNorm.astype(np.uint8)
+#     # xval = linearBezier(x1, x2, i/10)
+#     # yval = linearBezier(y1, y2, i/10)
 #
-# # plot result
-# plt.figure()
-# plt.imshow(zNormUint8)
+#     xval = quadraticBezier(x1, x2, x3, i/10)
+#     yval = quadraticBezier(y1, y2, y3, i/10)
+#
+#     print(xval)
+#
+#     x.append(xval)
+#     y.append(yval)
+#
+# # fig = plt.figure()
+# # ax = Axes3D(fig)
+# # ax.scatter(x, y)
+# plt.plot(x, y)
 # plt.show()
